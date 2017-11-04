9cb58f05c336bcfea7acb36dcbbc2808f22ea01e
Apurv Jyotirmay <apurvj199512@gmail.com>
2017-11-04 22:42:10 +0530

    Made some changes to make the purpose of the script clear.

--- critical_action.sh
+++ critical_action.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # This script is called when the battery is critically low
-# You can decide what action needs to be taken by adding the 
+# You can decide what action needs to be taken by adding the
 # instructions in this script.
 # By default, this script shuts down the computer.
+# Modify this script according to your needs and set necessary permissions.
 
 poweroff
--- critical_battery.sh
+++ critical_battery.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-#criticalLevel=9
+# This script monitors the battery for critically low power level and calls the necessary script.
 
 while true; do
-    STAT=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | grep "\(charging\|discharging\)" -o)
-    BAT=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | grep '[0-9]' -o)
-    if [ "$STAT" == 'discharging' ] && [ "$BAT" -lt $criticalLevel ]; then
-        # takes the action specified in this script
-        ./critical_action.sh
-        exit 0
-    fi
-    sleep 30s
+	STAT=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | grep "\(charging\|discharging\)" -o)
+	BAT=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | grep '[0-9]' -o)
+	if [ "$STAT" == 'discharging' ] && [ "$BAT" -lt $criticalLevel ]; then
+		# takes the action specified in this script
+		./critical_action.sh
+		exit 0
+	fi
+	sleep 30s
 done
-
