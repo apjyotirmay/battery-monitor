5b4b0304fcaafae10beec884bf658e196ccfcd5e
Apurv Jyotirmay <apurvj199512@gmail.com>
2017-11-04 22:39:00 +0530

    Adopted the defaults to play nicely with KDE Plasma 5.11

--- notification.sh
+++ notification.sh
@@ -4,21 +4,21 @@ status=$1
 
 if [ "$status" = '0' ]
 then
-    msg="Disconnect charger!!\nPower holding at"
-    icon="battery-full"
+	msg="Disconnect charger!!\nPower holding at"
+	icon="battery-full"
 elif [ "$status" = '1' ]
 then
-    msg="Disconnect charger!!\nPower holding at"
-    icon="battery-full"
+	msg="Disconnect charger!!\nPower holding at"
+	icon="battery-full"
 elif [ "$status" = 2 ]
 then
-    msg="Connect charger!!\nPower dropping below"
-    icon="battery-caution"
+	msg="Connect charger!!\nPower dropping below"
+	icon="battery-caution"
 fi
 
 while true
 do
-    BAT=$(upower -i $loc | grep percentage | grep '[0-9][0-9]' --only-matching)
-    echo -e "$msg: $BAT%" | xargs -d '\n' notify-send -t $timeout -i $icon -u normal
-    sleep $(( $timeout/1000 ))s
+	BAT=$(upower -i $loc | grep percentage | grep '[0-9][0-9]' --only-matching)
+	echo -e "$msg: $BAT%" | xargs -d '\n' notify-send -t $timeout -i $icon -u normal
+	sleep $(( $timeout/1000 ))s
 done
