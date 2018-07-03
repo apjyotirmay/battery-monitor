64a51bd4f819c8e44e6d94097c67d07326c87a12
Apurv Jyotirmay <apurvj199512@gmail.com>
2018-07-03 13:08:41 +0530

    Prepared the scripts acOnScript.sh and acOffScript.sh

---
+++ acOffScript.sh
@@ -0,0 +1,7 @@
+#!/bin/env bash
+
+# Enter the commands that you'd like to execute at the time external
+# AC source is disconnected from the system. Enter your commands or scripts
+# that you want to execute before the line saying "exit 0"
+
+exit 0
