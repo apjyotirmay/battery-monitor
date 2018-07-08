2840eac236da6390cfab2ba701ff0e8efe4dbf40
Apurv Jyotirmay <apurvj199512@gmail.com>
2018-07-08 19:42:06 +0530

    Cleaned up code & removed unnecessary lines, made it rely on central configuration

--- notification.sh
+++ notification.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
+source environmentVariables.sh
+
 status=$1
+
+# Codes:
+# 0 - Battery charging and more than maximum value of charge intended
+# 1 - Battery fully-charged
+# 2 - Battery is lower than the intended charge
 
 if [ "$status" = '0' ]
 then
-	msg="Disconnect charger!!\nPower holding at"
+	msg="Battery Monitor\nDisconnect charger!!"
 	icon="battery-full"
 elif [ "$status" = '1' ]
 then
-	msg="Disconnect charger!!\nPower holding at"
+	msg="Battery Monitor\nDisconnect charger!!"
 	icon="battery-full"
 elif [ "$status" = 2 ]
 then
-	msg="Connect charger!!\nPower dropping below"
+	msg="Battery Monitor\nConnect charger!!"
 	icon="battery-caution"
 fi
 
 if [ "$method" = "static" ]
 then            
-    BAT=$(upower -i $loc | grep percentage | grep '[0-9]*' --only-matching)
-    echo -e "$msg: $BAT%" | xargs -d '\n' notify-send -i $icon -u critical
+    echo -e "$msg" | xargs -d '\n' notify-send -i $icon -u critical
 elif [ "$method" = "flash" ]
 then
     while true
     do
-        BAT=$(upower -i $loc | grep percentage | grep '[0-9]*' --only-matching)
-        echo -e "$msg: $BAT%" | xargs -d '\n' notify-send -t $timeout -i $icon -u normal
+        echo -e "$msg" | xargs -d '\n' notify-send -t $timeout -i $icon -u normal
         sleep $(( $timeout/1000 ))s
     done
 fi
