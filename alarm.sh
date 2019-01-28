1d3118550d232aface011eb769f9eeeecd762935
Apurv Jyotirmay <apurvj199512@gmail.com>
2019-01-29 01:47:15 +0530

    Made a fix for when alarm isn't killed

--- alarm.sh
+++ alarm.sh
@@ -3,7 +3,29 @@
 # This script is called by the main script and sounds alarm under favourable conditions
 # To enable alarm, it needs to be turned on in the main script
 
+source environmentVariables.sh
+
+batteryStatus
+INITSTATE=$STAT
+
 while true
 do
-	paplay sound/alarm.oga
+    batteryStatus
+    if [ "$INITSTATE" != "$STAT" ]
+    then
+        exit
+    fi
+
+    SoundLevel=`pulseaudio-ctl current | sed 's/\%//'`
+
+    if [ "$SoundLevel"  -lt 50 ]
+    then
+        `paplay sound/alarm-00.ogg`
+    elif [ "$SoundLevel" -lt 80 ]
+    then
+        `paplay sound/alarm-10.ogg`
+    else
+        `paplay sound/alarm-20.ogg`
+    fi
+    sleep 2s
 done
